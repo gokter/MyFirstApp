a492d03889b213f350f371a5e9205d644a456cc7
flycoding <flycoding@yeah.net>
2012-12-29 20:53:36 +0800

    add onClick event handle, add another activity and start the new activity.

--- src/com/flyingh/myfirstapp/MainActivity.java
+++ src/com/flyingh/myfirstapp/MainActivity.java
@@ -1,22 +1,35 @@
 package com.flyingh.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	public static final String EXTRA_MESSAGE = "com.flyingh.myfirstapp.MESSAGE";
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		intent.putExtra(EXTRA_MESSAGE,
+				((EditText) findViewById(R.id.edit_message)).getText()
+						.toString());
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
